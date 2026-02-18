2eea7437d323fe900f41194343c7ce8c7703dd70
JP19-03 <u202014511@upc.edu.pe>
2026-02-18 17:49:44 -0500

    feat: implement Orders module with CreateOrderDto and UpdateOrderDto for order management

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Body, Param, Inject, ParseIntPipe } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+
+@Controller('orders')
+export class OrdersController {
+
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy
+  ) { }
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto)
+      .pipe(catchError(err => { throw new RpcException(err) }));
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send('findAllOrders', {})
+      .pipe(catchError(err => { throw new RpcException(err) }));
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersClient.send('findOneOrder', { id })
+      .pipe(catchError(err => { throw new RpcException(err) }));
+  }
+}
