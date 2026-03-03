98e284b4a4fa8b071ef2496716f2548844eecc0f
JP19-03 <u202014511@upc.edu.pe>
2026-03-03 11:07:01 -0500

    feat: add NatsModule for NATS messaging integration and update module imports

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [OrdersController],
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.NATS_SERVERS
-        }
-      }
-    ])
-  ]
+  imports: [NatsModule]
 })
 export class OrdersModule { }
